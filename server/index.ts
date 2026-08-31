eb9f4b37441a67d5a2eaf919e42abe77451252d5
Diar <diarselmani10@gmail.com>
2026-08-31 18:00:09 +0200

    port

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,7 @@
 import app from "./src/app.js";
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = Number(process.env.PORT) || 4000;
+
+app.listen(Number(PORT), "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
